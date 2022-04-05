9ce09f9639842de2f7b5fce75b019a5b46bb1146
Sangram Pradhan <60254473+Huksons1683@users.noreply.github.com>
2022-04-05 09:28:14 +0530

    Add files via upload

---
+++ equaloper.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class equaloper {
+	public static void main (String args[])  
+    { 
+	System.out.println(23==45);
+    }
+}
---
+++ if_ex.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class if_ex {
+	public static void main (String args[])  
+    { 
+		int  a = 55;
+		int b = 70;
+		System.out.println(a<50 && a<b);
+    }
+
+}
